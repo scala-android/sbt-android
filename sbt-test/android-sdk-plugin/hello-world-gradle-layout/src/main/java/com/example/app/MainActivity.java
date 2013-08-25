60c13bb682c844e860ef2fd312e5c1b48c579754
Perry Nguyen <pfnguyen@hanhuy.com>
2013-08-24 19:44:31 -0700

    add hello-world-gradle-layout test

---
+++ sbt-test/android-sdk-plugin/hello-world-gradle-layout/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.hello);
+    }
+}
