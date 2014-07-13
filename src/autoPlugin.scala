d022e2f9be49c6414d8e647705cec4beca0750ee
Perry Nguyen <pfnguyen@hanhuy.com>
2014-07-12 23:26:19 -0700

    Add aliases for AutoPlugin autoImport
    aliases added for ProguardCache, ProguardInputs, apklib, aar,
    AutoLibraryProject and LibraryProject

--- src/autoPlugin.scala
+++ src/autoPlugin.scala
@@ -1,10 +1,28 @@
 package android
 
+import sbt._
+
 object AndroidPlugin extends sbt.AutoPlugin {
 
   object autoImport extends KeysContainer {
     type ProjectLayout = android.ProjectLayout
-
     val ProjectLayout = android.ProjectLayout
+
+    type ProguardCache = android.ProguardCache
+    val ProguardCache = android.ProguardCache
+    type ProguardInputs = android.ProguardInputs
+    val ProguardInputs = android.ProguardInputs
+
+    import Dependencies.artifacts
+    def apklib(m: ModuleID, n: String): ModuleID = artifacts(m, n, "apklib")
+    def apklib(m: ModuleID): ModuleID            = artifacts(m, m.name, "apklib")
+    def aar(m: ModuleID, n: String): ModuleID    = artifacts(m, n, "aar")
+    def aar(m: ModuleID): ModuleID               = artifacts(m, m.name, "aar")
+
+    val AutoLibraryProject = Dependencies.AutoLibraryProject
+    type AutoLibraryProject = Dependencies.AutoLibraryProject
+
+    type LibraryProject = Dependencies.LibraryProject
+    val LibraryProject = Dependencies.LibraryProject
   }
 }
