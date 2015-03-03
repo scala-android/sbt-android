f380cfcf4d22ce7bb1be2e68c106bca733e1fded
Perry Nguyen <pfnguyen@hanhuy.com>
2015-03-02 18:43:19 -0800

    Mention that android:manifest should be readonly

--- src/keys.scala
+++ src/keys.scala
@@ -130,7 +130,8 @@ object Keys {
   val packageName = TaskKey[String]("package-name",
     "android package name, can be changed to create a different apk package")
   // TODO turn this and all dependents into a TaskKey, manifest can change
-  val manifest = TaskKey[Elem]("manifest", "android manifest xml object")
+  val manifest = TaskKey[Elem]("manifest",
+    "android manifest xml object, read-only, do not modify")
   val manifestPlaceholders = TaskKey[Map[String,String]](
     "manifest-placeholders", "${variable} expansion for AndroidManifest.xml")
   val packageForR = TaskKey[String]("packageForR",
