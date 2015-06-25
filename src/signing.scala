4e552d6865ddcc309381254c9336dfae4291eb5d
Perry Nguyen <pfnguyen@hanhuy.com>
2015-06-25 12:29:04 -0700

    make key prompt only once per session

--- src/signing.scala
+++ src/signing.scala
@@ -18,7 +18,7 @@ case class PlainSigningConfig(override val keystore: File,
 case class PromptStorepassSigningConfig(override val keystore: File,
                                         override val alias: String,
                                         override val storeType: String = "jks") extends ApkSigningConfig {
-  override def storePass =
+  override lazy val storePass =
     System.console.readPassword("Enter keystore password: ") mkString
 }
 
@@ -26,8 +26,8 @@ case class PromptPasswordsSigningConfig(override val keystore: File,
                                         override val alias: String,
                                         override val storeType: String = "jks") extends ApkSigningConfig {
   import System.console
-  override def storePass =
+  override lazy val storePass =
     console.readPassword("Enter keystore password: ") mkString
-  override def keyPass =
+  override lazy val keyPass =
     Option(console.readPassword("Enter password for key '%s': " format alias) mkString)
 }
