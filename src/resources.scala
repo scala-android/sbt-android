68cc6d534c7acc0c51af3abd74138f10a338d0aa
Niklas Klein <mail@taig.io>
2016-04-28 19:29:02 -0500

    Inject deprecation forwarder depending on platformApi

--- src/resources.scala
+++ src/resources.scala
@@ -405,6 +405,39 @@ object Resources {
                |  }""".stripMargin :: acc
           }
 
+          val deprForward = {
+            if (platformApi < 21) ""
+            else {
+              val color =
+                """
+                  |    def getColor(c: Context, resid: Int): Int = {
+                  |      if (Build.VERSION.SDK_INT >= 23)
+                  |        c.getColor(resid)
+                  |      else
+                  |        c.getResources.getColor(resid)
+                  |    }""".stripMargin
+              val drawable =
+               """
+                  |    def getDrawable(c: Context, resid: Int): Drawable = {
+                  |      if (Build.VERSION.SDK_INT >= 21)
+                  |        c.getDrawable(resid)
+                  |      else
+                  |        c.getResources.getDrawable(resid)
+                  |    }""".stripMargin
+
+              val methods = if (platformApi >= 23) color + "\n\n" + drawable else drawable
+
+              s"""
+                |  // Helper object to suppress deprecation warnings as discussed in
+                |  // https://issues.scala-lang.org/browse/SI-7934
+                |  @deprecated("", "")
+                |  private trait compat {
+                |${methods}
+                |  }
+                |  private object compat extends compat""".stripMargin
+            }
+          }
+
           val getColor = if (platformApi >= 23) {
             "compat.getColor(c,resid)"
           } else {
@@ -422,7 +455,7 @@ object Resources {
             } mkString "\n",
             layoutTypes map { case (k,v) =>
               "    final val %s = TypedLayout[%s](R.layout.%s)" format (wrap(k),v,wrap(k))
-            } mkString "\n", trs.mkString, getColor, getDrawable, getDrawable))
+            } mkString "\n", trs.mkString, getColor, getDrawable, getDrawable, deprForward))
           Set(tr)
         } else Set.empty
       }(a.toSet).toSeq
