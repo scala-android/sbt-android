e1176468b34a1788fa09037242eb675cab4b4bf4
Perry Nguyen <pfnguyen@hanhuy.com>
2016-09-08 00:35:11 -0700

    Move aar packaging settings into AndroidLib

--- src/AndroidLibSettings.scala
+++ src/AndroidLibSettings.scala
@@ -3,10 +3,16 @@ package android
 import Keys._, Internal._
 
 import sbt._
+import sbt.Keys._
 /**
   * @author pfnguyen
   */
 trait AndroidLibSettings extends AutoPlugin {
-  override def projectSettings = Seq(libraryProject := true) ++
+  override def projectSettings = Seq(
+    aarArtifact            <<= normalizedName { n => Artifact(n, "aar", "aar") },
+    packageAar             <<= Tasks.packageAarTaskDef,
+    mappings in packageAar <<= Tasks.packageAarMappings,
+    libraryProject          := true
+  ) ++
     addArtifact(aarArtifact , packageAar)
 }
