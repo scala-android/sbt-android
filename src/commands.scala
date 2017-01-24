e526313256fd27c16ca75808948bb4cdce33abad
Perry Nguyen <pfnguyen@hanhuy.com>
2017-01-24 09:44:11 -0800

    fix deprecation message in gen-android build script

--- src/commands.scala
+++ src/commands.scala
@@ -386,7 +386,7 @@ object Commands {
     val buildSettings =
       """scalaVersion := "2.11.8"""" ::
       "" ::
-      "enablePlugins(AndroidApp)" :: "useSupportVectors" ::
+      "enablePlugins(AndroidApp)" :: "android.useSupportVectors" ::
       "" ::
       "versionCode := Some(1)" ::
       """version := "0.1-SNAPSHOT"""" ::
